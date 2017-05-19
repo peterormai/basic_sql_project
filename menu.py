d11a4b1cf706bbf0054337b9fa00d3e5febd148b
peterormai <ormaipeter.fx@gmail.com>
2017-05-19 16:37:25 +0200

    change options from 9 to 7

--- menu.py
+++ menu.py
@@ -10,11 +10,9 @@ def print_query_menu():
     print("2: Nick names of the mentors at Miskolc")
     print("3: Full name and phone number of Carol the applicant")
     print("4: Full name and phone number of Applicant from Adipiscingenimmi")
-    print("5: Add Markus Schaffarzyk to the Applicants")
-    print("6: Show details of Markus Schaffarzyk the Applicant")
-    print("7: Update phone number of Jemina Foreman")
-    print("8: Show phone number of Jemina Foreman")
-    print("9: Delete Applicants with @mauriseu.net email adress ending" + "\n")
+    print("5: Add Markus Schaffarzyk to the Applicants and show his details")
+    print("6: Update & Show the new phone number of Jemina Foreman")
+    print("7: Delete Applicants with @mauriseu.net email adress ending" + "\n")
 
 
 def query_menu(opened_con):
@@ -24,23 +22,22 @@ def query_menu(opened_con):
         try:
             chosen_option = int(input("Choose an option: "))
             os.system('clear')
-            valid_inputs = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+            valid_inputs = [1, 2, 3, 4, 5, 6, 7]
             if chosen_option in valid_inputs:
-                options = {1: queries.mentor_names,
-                           2: queries.miskolc_mentor_nicknames,
-                           3: queries.carol_name_and_phonenum,
-                           4: queries.girl_from_adipiscingenimmi,
-                           5: queries.add_new_applicant,
-                           6: queries.show_new_applicant,
-                           7: queries.update_phone_num,
-                           8: queries.show_jemina_phone,
-                           9: queries.del_mauriseu}
-                query_handler.query_result(opened_con, options[chosen_option]())
+                options = {1: [queries.mentor_names()],
+                           2: [queries.miskolc_mentor_nicknames()],
+                           3: [queries.carol_name_and_phonenum()],
+                           4: [queries.girl_from_adipiscingenimmi()],
+                           5: [queries.add_new_applicant(), queries.show_new_applicant()],
+                           6: [queries.update_phone_num(), queries.show_jemina_phone()],
+                           7: [queries.del_mauriseu()]}
+                for func in options[chosen_option]:
+                    query_handler.query_result(opened_con, func)
             elif chosen_option == 0:
                 running_app = False
             else:
                 os.system('clear')
-                print('Invalid input! Please select from these options: 0,1,2,3,4,5,6,7,8,9')
+                print('Invalid input! Please select from these options: 0,1,2,3,4,5,6,7')
         except ValueError:
             os.system('clear')
-            print('Invalid input! Please select from these options: 0,1,2,3,4,5,6,7,8,9')
+            print('Invalid input! Please select from these options: 0,1,2,3,4,5,6,7')
