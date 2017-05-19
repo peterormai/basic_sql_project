193ea293e6066536a2919bed4c570de1e8ec614e
peterormai <ormaipeter.fx@gmail.com>
2017-05-19 13:49:42 +0200

    new solution: query orders outsourced into functions + new query result function

--- main.py
+++ main.py
@@ -13,81 +13,59 @@ def open_con():
         print(e)
 
 
-# def printer(cursor):
-#     rows = cursor.fetchall()
-#     print("RESULT:" + "\n")
-#     for row in rows:
-#         print(" ".join(map(str, row)))
-
 def query_result(conn, query):
     cursor = conn.cursor()
     cursor.execute(query)
-    rows = cursor.fetchall()
-    print("RESULT:" + "\n")
-    for row in rows:
-        print(" ".join(map(str, row)))
+    try:
+        rows = cursor.fetchall()
+        print("RESULT:" + "\n")
+        for row in rows:
+            print(" ".join(map(str, row)))
+    except:
+        print("DONE!" + "\n")
 
 
 def mentor_names():
     return """SELECT first_name,last_name FROM mentors;"""
 
 
-# def mentor_names(conn):
-#     cursor = conn.cursor()
-#     cursor.execute("""SELECT first_name,last_name FROM mentors;""")
-#     printer(cursor)
+def miskolc_mentor_nicknames():
+    return """SELECT nick_name FROM mentors;"""
 
 
-def miskolc_mentor_nicknames(conn):
-    cursor = conn.cursor()
-    cursor.execute("""SELECT nick_name FROM mentors;""")
-    printer(cursor)
+def carol_name_and_phonenum():
+    return """SELECT CONCAT(first_name, ' ', last_name)
+                   AS full_name,phone_number FROM applicants WHERE first_name='Carol';"""
 
 
-def carol_name_and_phonenum(conn):
-    cursor = conn.cursor()
-    cursor.execute("""SELECT CONCAT(first_name, ' ', last_name)
-                   AS full_name,phone_number FROM applicants WHERE first_name='Carol';""")
-    printer(cursor)
+def girl_from_adipiscingenimmi():
+    return """SELECT CONCAT(first_name, ' ', last_name) AS full_name, phone_number
+                   FROM applicants WHERE email LIKE '%@adipiscingenimmi.edu';"""
 
 
-def girl_from_adipiscingenimmi(conn):
-    cursor = conn.cursor()
-    cursor.execute("""SELECT CONCAT(first_name, ' ', last_name) AS full_name, phone_number
-                   FROM applicants WHERE email LIKE '%@adipiscingenimmi.edu';""")
-    printer(cursor)
-
-
-def add_new_applicant(conn):
-    cursor = conn.cursor()
-    cursor.execute("""INSERT INTO applicants(first_name, last_name, phone_number, email, application_code)
+def add_new_applicant():
+    return """INSERT INTO applicants(first_name, last_name, phone_number, email, application_code)
                    SELECT 'Markus', 'Schaffarzyk', '003620/725-2666', 'djnovus@groovecoverage.com', 54823
-                   WHERE NOT EXISTS(SELECT * FROM applicants WHERE application_code=54823);""")
+                   WHERE NOT EXISTS(SELECT * FROM applicants WHERE application_code=54823);"""
 
 
-def show_new_applicant(conn):
-    cursor = conn.cursor()
-    cursor.execute("""SELECT * FROM applicants WHERE application_code=54823;""")
-    printer(cursor)
+def show_new_applicant():
+    return """SELECT * FROM applicants WHERE application_code=54823;"""
 
 
-def update_phone_num(conn):
-    cursor = conn.cursor()
-    cursor.execute("""UPDATE applicants SET phone_number='003670/223-7459'
-                   WHERE first_name='Jemima' AND last_name='Foreman';""")
+def update_phone_num():
+    return """UPDATE applicants SET phone_number='003670/223-7459'
+                   WHERE first_name='Jemima' AND last_name='Foreman';"""
 
 
-def show_jemina_phone(conn):
-    cursor = conn.cursor()
-    cursor.execute("""SELECT phone_number FROM applicants
-                   WHERE first_name='Jemima' AND last_name='Foreman';""")
-    printer(cursor)
+def show_jemina_phone():
+    return """SELECT phone_number FROM applicants
+                   WHERE first_name='Jemima' AND last_name='Foreman';"""
 
 
-def del_mauriseu(conn):
-    cursor = conn.cursor()
-    cursor.execute("""DELETE FROM applicants
-                   WHERE email LIKE '%@mauriseu.net';""")
+def del_mauriseu():
+    return """DELETE FROM applicants
+                   WHERE email LIKE '%@mauriseu.net';"""
 
 
 def main():
@@ -120,7 +98,6 @@ def main():
                            7: update_phone_num,
                            8: show_jemina_phone,
                            9: del_mauriseu}
-                # options[chosen_option](opened_con)
                 query_result(opened_con, options[chosen_option]())
             elif chosen_option == 0:
                 running_app = False
